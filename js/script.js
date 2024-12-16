2436a14205b4ec4dde594b55add6864e60aae914
miablb <marianna22.02.2005@gmail.com>
2024-12-16 19:39:45 +0300

    transfer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4.7,
+    spaceBetween: 5,
+    loop: true,
+    fade: true,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    }
+});
